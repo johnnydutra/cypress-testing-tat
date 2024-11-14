e095afc83733be4532203f88be06ac37174b198f
Johnny Dutra <johnny.dutra@gmail.com>
2024-11-14 11:44:45 -0300

    sets baseurl

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://wlsf82-hacker-stories.web.app',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'https://wlsf82-hacker-stories.web.app'
   },
 });
