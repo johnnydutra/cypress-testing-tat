5bd7a9e077499e774eeb3ab8d4e76deeac392758
Johnny Dutra <johnny.dutra@gmail.com>
2024-11-14 05:04:27 -0300

    updates config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: 'http://localhost',
+    env: {
+      hideCredentials: true,
+      requestMode: true
     },
+    experimentalRunAllSpecs: true
   },
+  fixturesFolder: false,
+  video: false
 });
