1f761f515c974aa5ffe107f79947508501fed96d
Johnny Dutra <johnny.dutra@gmail.com>
2024-11-14 05:04:39 -0300

    adds types declaration

---
+++ cypress/support/types.d.ts
@@ -0,0 +1,20 @@
+interface Project {
+  projectId?: string
+  name: string
+  description: string
+}
+
+interface Issue {
+  title: string
+  description: string
+  project: Project
+}
+
+interface Label {
+  name: string
+  color: string
+}
+
+interface Milestone {
+  title: string
+}
